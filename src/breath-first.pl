f21c1740037c222efbf98189ee646378d80bff8f
devSaraiva <bernardosaraiva1503@gmail.com>
2022-01-02 20:58:07 +0000

    breath feito

--- src/breath-first.pl
+++ src/breath-first.pl
@@ -9,9 +9,10 @@
 goal(gualtar/green-distribution).
 
 
-bfs(Nodo,Caminho):-
+bfs(Nodo,Caminho,Custo):-
     goal(Final),
-    bfs1(Final,[[Nodo]],Caminho).
+    bfs1(Final,[[Nodo]],Caminho),
+    calculaCusto(Caminho,Custo).
 
 bfs1(Final,[[Final|XS]|_],Caminho):- reverse([Final|XS],Caminho).
 
@@ -21,3 +22,13 @@ bfs1(Final,[Atuais|Outros],Caminho):-
     findall([X|Atuais], (aresta(Act,X,C1) ,not(member(X,Atuais))),Novos),
     append(Outros,Novos,Todos),
     bfs1(Final,Todos,Caminho).
+
+
+
+calculaCusto([X,Y|[]],C1):-
+    aresta(X,Y,C1).
+
+calculaCusto([X,Y|XS],Custo):-
+    aresta(X,Y,C1),
+    calculaCusto([Y|XS],C2),
+    Custo is C1 + C2.
