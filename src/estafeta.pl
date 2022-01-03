055efa63065e946be64766b5567c74077e3cd847
jjgonc <josejoao_2001@hotmail.com>
2022-01-03 18:02:26 +0000

    estafetas atualizados

--- src/estafeta.pl
+++ src/estafeta.pl
@@ -1,8 +1,28 @@
 % estafeta(idEstafeta, freguesia, veiculo)
-estafeta(joaquim, vila-caiz, carro).
-estafeta(painatal, roriz, carro).
-estafeta(rui, roriz, mota).
-estafeta(ze-joao, barcelos, bicicleta).
-estafeta(miguel, braga, bicicleta).
-estafeta(margarida, guimaraes, mota).
-estafeta(gigachad, braga, bicicleta).
+estafeta(bobbaFett, palmeira, carro).
+estafeta(yoda, palmeira, mota).
+estafeta(stormtrooper, palmeira, bicicleta).
+
+estafeta(jangoFett, maximinos, carro).
+estafeta(dathVader, maximinos, mota).
+estafeta(luke, maximinos, bicicleta).
+
+estafeta(r2d2, real, carro).
+estafeta(rui, real, mota).
+estafeta(leia, real, bicicleta).
+
+estafeta(anakin, sao-vitor, carro).
+estafeta(chewbacca, sao-vitor, mota).
+estafeta(ze-joao, sao-vitor, bicicleta).
+
+estafeta(c3po, gualtar, carro).
+estafeta(bb8, gualtar, mota).
+estafeta(miguel, gualtar, bicicleta).
+
+estafeta(hanSolo, nogueiro, carro).
+estafeta(margarida, nogueiro, mota).
+estafeta(mandalorian, nogueiro, bicicleta).
+
+estafeta(maceWindu, lamacaes, carro).
+estafeta(darthMaul, lamacaes, mota).
+estafeta(gigachad, lamacaes, bicicleta).
