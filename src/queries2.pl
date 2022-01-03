a6c5973c8af80dbae5c5d1ac2466fb63c88d323e
jjgonc <josejoao_2001@hotmail.com>
2022-01-03 23:44:58 +0000

    removed duplicated fragment

--- src/queries2.pl
+++ src/queries2.pl
@@ -81,26 +81,6 @@ calculaOcorrencias(X, [Y|XS], Res) :-
 
 
 
-%decrescimos
-
-decrescimo_bicicleta(VelocidadeMedia, Kgs, NovaVelocidadeMedia) :-
-    Decrescimo is 0.7 * Kgs,
-    NovaVelocidadeMedia is VelocidadeMedia - Decrescimo.
-
-decrescimo_motos(VelocidadeMedia, Kgs, NovaVelocidadeMedia) :-
-    Decrescimo is 0.5 * Kgs,
-    NovaVelocidadeMedia is VelocidadeMedia - Decrescimo.
-
-decrescimo_carro(VelocidadeMedia, Kgs, NovaVelocidadeMedia) :-
-    Decrescimo is 0.1 * Kgs,
-    NovaVelocidadeMedia is VelocidadeMedia - Decrescimo.
-
-
-
-calcularEcologiaPorTempo(Distancia, Decrescimo, Tempo) :-    %o Decrescimo vem do predicado decrescimo_motos / bicicleta / carro
-    Tempo is Distancia/Decrescimo.
-
-
 
 indicadorDeProdutividade(Veiculo, Caminho, Tempo, Res) :-
     calculaCusto(Caminho, TotalDist),
