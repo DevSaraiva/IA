f4acfb76175fdc55744f7ba4ad11f43863d14d75
jjgonc <josejoao_2001@hotmail.com>
2022-01-03 19:02:56 +0000

    funcao de produtividade & alteradas funcoes para queries2.pl

--- src/queries2.pl
+++ src/queries2.pl
@@ -28,3 +28,24 @@ evolucao( Termo ):- (
 
 
 
+decrescimo_bicicleta(VelocidadeMedia, Kgs, NovaVelocidadeMedia) :-
+    Decrescimo is 0.7 * Kgs,
+    NovaVelocidadeMedia is VelocidadeMedia - Decrescimo.
+
+decrescimo_motos(VelocidadeMedia, Kgs, NovaVelocidadeMedia) :-
+    Decrescimo is 0.5 * Kgs,
+    NovaVelocidadeMedia is VelocidadeMedia - Decrescimo.
+
+decrescimo_carro(VelocidadeMedia, Kgs, NovaVelocidadeMedia) :-
+    Decrescimo is 0.1 * Kgs,
+    NovaVelocidadeMedia is VelocidadeMedia - Decrescimo.
+
+
+
+calcularEcologiaPorTempo(Distancia, Decrescimo, Tempo) :-    %o Decrescimo vem do predicado decrescimo_motos / bicicleta / carro
+    Tempo is Distancia/Decrescimo.
+
+
+
+indicadorDeProdutividade(Distancia, Tempo, Res) :-
+    Res is (Distancia+Tempo). 
