e915edd4d0ccbf45a753f3ade3d4497dc7942270
jjgonc <josejoao_2001@hotmail.com>
2022-01-05 23:54:50 +0000

    numeros da main alterados e 1a opçao eliminada

--- src/main.pl
+++ src/main.pl
@@ -8,37 +8,33 @@
 
 menu :- repeat, nl, nl, nl,
 	write('________________________________________________ Sistema de Recomendação de Circuitos de Entrega de Encomendas ______________________________________'), nl, nl,
-	write(' 1 - Gerar os circuitos de entrega, caso existam, que cubram um determinado território'), nl,
-	write(' 2 - Identificar quais os circuitos com maior número de entregas (por volume e peso)'), nl,
-	write(' 3 - Comparar circuitos de entrega tendo em conta os indicadores de produtividade'), nl,
-	write(' 4 - Escolher o circuito mais rápido (usando o critério da distância)'), nl,
-	write(' 5 - Escolher o circuito mais ecológico (usando um critério de tempo)'), nl,
-	write(' 6 - Gerar um circuito de entrega com algoritmo à escolha'), nl,
-	write(' 7 - Adicionar uma encomenda'), nl,
-	write(' 8 - Mostrar encomendas não entregues'), nl,
-	write(' 9- Sair.'), nl, nl,
+	write(' 1 - Identificar quais os circuitos com maior número de entregas (por volume e peso)'), nl,
+	write(' 2 - Comparar circuitos de entrega tendo em conta os indicadores de produtividade'), nl,
+	write(' 3 - Escolher o circuito mais rápido (usando o critério da distância)'), nl,
+	write(' 4 - Escolher o circuito mais ecológico (usando um critério de tempo)'), nl,
+	write(' 5 - Gerar um circuito de entrega com algoritmo à escolha'), nl,
+	write(' 6 - Adicionar uma encomenda'), nl,
+	write(' 7 - Mostrar encomendas não entregues'), nl,
+	write(' 8- Sair.'), nl, nl,
     write('Digite a opção:'), nl,
 	read(Choice), runQuery(Choice), menu.
 
 
-% FALTA FAZER
-runQuery(1) :- 
-	write("Digite o território que quer analisar: "),
-	read(Territorio).
 
-runQuery(2) :- 
+
+runQuery(1) :- 
 	write("Digite o número de circuitos que pretende: "),
 	read(NumeroCircuitos),
 	circuitosComMaisEntregas(NumeroCircuitos, Solucao),
 	write(Solucao).
 
-runQuery(3) :- 
+runQuery(2) :- 
 	write("Digite o número de circuitos que prentende: "),
 	read(NumCircuitos),
 	circuitosComMaiorProdutividade(NumCircuitos, Sol),
 	write(Sol).
 
-runQuery(4) :- 
+runQuery(3) :- 
 	write("Insira o ano referente à data de inicio: "), read(AnoPrazo), nl,
 	write("Insira o mes referente à data de inicio"), read(MesPrazo), nl,
 	write("Insira o dia referente à data de inicio"), read(DiaPrazo), nl,
@@ -48,7 +44,7 @@ runQuery(4) :-
 	read(IdEncomenda),nl,
 	escolherCircuitoMaisRapido(data(AnoPrazo, MesPrazo, DiaPrazo)/hora(HoraPrazo, MinPrazo), IdEncomenda).
 
-runQuery(5) :- 
+runQuery(4) :- 
 	write("Insira o ano referente à data de inicio: "), read(AnoPrazo), nl,
 	write("Insira o mes referente à data de inicio"), read(MesPrazo), nl,
 	write("Insira o dia referente à data de inicio"), read(DiaPrazo), nl,
@@ -58,7 +54,7 @@ runQuery(5) :-
 	read(IdEncomenda),nl,
 	escolherCircuitoMaisEcologico(data(AnoPrazo, MesPrazo, DiaPrazo)/hora(HoraPrazo, MinPrazo), IdEncomenda).
 
-runQuery(6) :- 
+runQuery(5) :- 
 	write("1 - A Estrela com criterio distancia."), nl,
 	write("2 - A Estrela com criterio tempo."), nl,
 	write("3 - Gulosa com estima pela distancia."), nl,
@@ -72,7 +68,7 @@ runQuery(6) :-
 	escolheAlgoritmo(NumeroAlg, Nodo, Circuito/NovoCusto),
 	write(Circuito/NovoCusto).
 
-runQuery(7) :- 
+runQuery(6) :- 
 	write("Insira a morada no formato Freguesia/Rua: "), read(Freguesia/Rua), nl,
 	write("Insira o ID (único) da encomenda: "), read(IdEncomenda), nl,
 	write("Insira o ID do cliente: "), read(IdCliente), nl,
@@ -87,13 +83,13 @@ runQuery(7) :-
 	write("Encomenda com o ID "), write(IdEncomenda), write(" adicionada com sucesso! Existem agora "),
 	contaEncomendas(NEncomendas), write(NEncomendas), write(" encomendas.").
 
-runQuery(8) :- 
+runQuery(7) :- 
 	write("A calcular as encomendas que ainda não foram entregues..."), nl,
 	encomendasPorEntregar(EncomendasPorEntregar),
 	write(EncomendasPorEntregar). 
 
 %predicado que termina o programa
-runQuery(11) :- 
+runQuery(8) :- 
 	writeln("Até breve!"), nl, halt.
 
 %opcao final que só ocorre se nao entrar em nenhum dos casos anteriores
